b15bd7280d6898fbd094c340ff3c1e2c909975ea
Agampng <mahisaagampng@gmail.com>
2020-12-17 07:47:52 +0700

    filter by type

--- src/pages/home.js
+++ src/pages/home.js
@@ -11,18 +11,14 @@ class Home extends Component {
       prev: null,
       next: "pokemon",
       isLoaded: false,
+      itemTypes: [],
+      type: null,
     };
   }
   getAllPoke(next) {
     axios
       .get(next)
       .then((res) => {
-        console.log(
-          res.data.results[0].url.substring(
-            res.data.results[0].url.length - 2,
-            res.data.results[0].url.length - 1
-          )
-        );
         this.setState({
           items: res.data.results,
           next: res.data.next,
@@ -33,21 +29,90 @@ class Home extends Component {
       .catch();
   }
 
+  getTypes() {
+    axios
+      .get("type")
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          itemTypes: res.data.results,
+        });
+      })
+      .catch();
+  }
+
+  getPokeByTypes(type) {
+    axios
+      .get(`type/${type}`)
+      .then(async (res) => {
+        let typeItem = [];
+        await res.data.pokemon.map((item) => {
+          typeItem.push(item.pokemon);
+        });
+        this.setState({
+          items: typeItem,
+          next: null,
+          prev: null,
+          isLoaded: true,
+        });
+      })
+      .catch();
+  }
+
+  onChange = (e) => {
+    console.log(e.target.value);
+    this.setState({ type: e.target.value, isLoaded: false }, () => {
+      if (e.target.value) this.getPokeByTypes(this.state.type);
+    });
+  };
+
   changePage(url) {
     this.getAllPoke(url.substring(26));
   }
 
   componentDidMount() {
     this.getAllPoke(this.state.next);
+    this.getTypes();
   }
 
   render() {
     const { isLoaded, items, next, prev } = this.state;
     if (!isLoaded) {
-      return <div className="row">Loading..</div>;
+      return (
+        <div className="container">
+          <div className="row">Loading..</div>
+        </div>
+      );
     } else {
       return (
         <div className="container">
+          <div className={"row mt-4"}>
+            <h2>Filter</h2>
+            <div className="col-md-3 col-sm-12">
+              <select
+                className="form-control"
+                disabled={this.state.isLoad}
+                onChange={this.onChange}
+                name="filter"
+                value={this.state.type}
+              >
+                <option key={"load-role"} value="all">
+                  All Types
+                </option>
+                {this.state.itemTypes.map((item) => (
+                  <option
+                    name="types"
+                    onChange={this.onChange}
+                    value={item.name}
+                    key={item.name}
+                  >
+                    {item.name}
+                  </option>
+                ))}
+                );
+              </select>
+            </div>
+          </div>
           <div className="row m-4">
             {items.map((item) => (
               <div key={item.name} className="col-md-4 text-center my-4">
@@ -60,11 +125,10 @@ class Home extends Component {
                   <div className="card">
                     <img
                       className="card-img-top"
-                      src={`https://pokeres.bastionbot.org/images/pokemon/${item.url.substring(
-                        34,
-                        item.url.length - 1
-                      )}.png`}
-                      alt="Card image cap"
+                      src={`https://pokeres.bastionbot.org/images/pokemon/${
+                        item.url.substring(34, item.url.length - 1) ?? ""
+                      }.png`}
+                      alt="pokemon image"
                     />
                     <div className="card-body">
                       <p className="card-text font-weight-bold text-uppercase">
@@ -90,13 +154,17 @@ class Home extends Component {
               ) : (
                 ""
               )}
-              <button
-                type="button"
-                onClick={() => this.changePage(next)}
-                className="btn color-primary btn-sm mr-1"
-              >
-                Next
-              </button>
+              {next ? (
+                <button
+                  type="button"
+                  onClick={() => this.changePage(next)}
+                  className="btn color-primary btn-sm mr-1"
+                >
+                  Next
+                </button>
+              ) : (
+                ""
+              )}
             </div>
           </div>
         </div>
